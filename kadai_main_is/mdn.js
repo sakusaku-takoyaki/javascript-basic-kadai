28cb0c4d00fea3645257544816c7c1be46c230c6
sakusaku-takoyaki <sakuyasu20@gmail.com>
2024-07-01 16:38:23 +0900

    Add files via upload

---
+++ kadai_main_is/mdn.js
@@ -0,0 +1,6 @@
+const today = new Date(2023,8,23);
+const year = today.getFullYear();
+const month = today.getMonth(); 
+const day = today.getDate(); 
+
+console.log(year + '年' + month + '月' + day + '日');
